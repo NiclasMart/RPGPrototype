193b48322774d35c7a33a05651d793341e7bd801
Niclas Mart <salcintram07@web.de>
2021-04-26 09:44:19 +0200

    added projectile trail and created archer

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,6 +9,7 @@ namespace RPG.Combat
   public class Projectile : MonoBehaviour
   {
     [SerializeField] float speed = 1f;
+    [SerializeField] float destroyDelay = 0.1f;
 
     Health target;
     float damage;
@@ -32,7 +33,7 @@ namespace RPG.Combat
     private void ApplyDamage()
     {
       target.ApplyDamage(damage);
-      Destroy(gameObject);
+      Destroy(gameObject, destroyDelay);
     }
 
     private void OnTriggerEnter(Collider other)
