a323890a22e1c48c61e51a58d70e97cb5bbab3bd
Niclas Mart <salcintram07@web.de>
2021-05-01 11:28:29 +0200

    implemented base stats system

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+  public class BaseStats : MonoBehaviour
+  {
+    [SerializeField, Range(1, 99)] int level = 1;
+    [SerializeField] CharakterClass charakterClass;
+  }
+}
---
+++ Assets/Scripts/Stats/CharacterClass.cs
@@ -0,0 +1,10 @@
+namespace RPG.Stats
+{
+  public enum CharakterClass
+  {
+    PLAYER,
+    SOLDIERE,
+    ARCHER,
+    MAGE
+  }
+}
