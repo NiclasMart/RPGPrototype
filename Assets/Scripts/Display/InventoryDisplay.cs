b2494aa1aa2fc9a5eaf17f366cd2d318bd17da8c
Niclas Mart <salcintram07@web.de>
2021-06-03 11:11:15 +0200

    build basic chest UI end implemented inventory toggle

--- Assets/Scripts/Display/InventoryDisplay.cs
+++ Assets/Scripts/Display/InventoryDisplay.cs
@@ -6,10 +6,23 @@ namespace RPG.Display
 {
   public class InventoryDisplay : MonoBehaviour
   {
+    [SerializeField] KeyCode displayButton = KeyCode.I;
     [SerializeField] ItemDisplaySlot itemSlot;
     [SerializeField] RectTransform list;
     [SerializeField] TextMeshProUGUI capacityDisplay;
-    [HideInInspector] public ItemDisplaySlot currentlySelectedSlot {get; private set;}
+    [HideInInspector] public ItemDisplaySlot currentlySelectedSlot { get; private set; }
+
+    bool displayActive = false;
+
+    private void Awake()
+    {
+      transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+      ToggleDisplay();
+    }
 
     public void AddNewItemToDisplay(Sprite sprite, string itemID)
     {
@@ -33,6 +46,15 @@ namespace RPG.Display
     {
       if (!currentlySelectedSlot) return;
       Destroy(currentlySelectedSlot.gameObject);
+    }
+
+    private void ToggleDisplay()
+    {
+      if (Input.GetKeyDown(displayButton))
+      {
+        displayActive = !displayActive;
+        transform.GetChild(0).gameObject.SetActive(displayActive);
+      }
     }
   }
 }
