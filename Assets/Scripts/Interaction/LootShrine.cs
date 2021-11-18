a3662d69a4fc7bacc786dd1aa577387cff46e19a
Niclas Mart <salcintram07@web.de>
2021-11-18 10:22:50 +0100

    implemented saving for loot shrine and added color indicator for capacity level

--- Assets/Scripts/Interaction/LootShrine.cs
+++ Assets/Scripts/Interaction/LootShrine.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 
 namespace RPG.Interaction
 {
-  public class LootShrine : Interactable
+  public class LootShrine : Interactable, ISaveable
   {
+    [SerializeField] Gradient colorSchema = new Gradient();
     [SerializeField] LootBank connectedBank;
     Light showLight;
 
     private void Awake()
     {
       showLight = GetComponentInChildren<Light>();
+      float capacityLevel = connectedBank.GetCapacityLevel();
+      showLight.color = colorSchema.Evaluate(capacityLevel);
       showLight.enabled = !connectedBank.Empty;
     }
 
@@ -22,6 +25,27 @@ namespace RPG.Interaction
       PlayerInventory playerInventory = interacter.GetComponent<Interacter>().mainInventory;
       playerInventory.AddItems(connectedBank.GetLoot());
       FindObjectOfType<SavingSystem>().Save("PlayerData", SaveType.PlayerSpecific);
+    }
+
+    public object CaptureSaveData(SaveType saveType)
+    {
+      List<object> saveData = new List<object>();
+      foreach (var item in connectedBank.ShowLoot())
+      {
+        saveData.Add(item.GetSaveData());
+      }
+      return saveData;
+    }
+
+    public void RestoreSaveData(object data)
+    {
+      connectedBank.ClearLoot();
+
+      List<object> saveData = (List<object>)data;
+      foreach (object obj in saveData)
+      {
+        connectedBank.AddLoot((obj as Item.SaveData).CreateItemFromData());
+      }
     }
   }
 }
--- Assets/Scripts/Items/LootBank.cs
+++ Assets/Scripts/Items/LootBank.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Items
@@ -7,18 +8,46 @@ namespace RPG.Items
   [CreateAssetMenu(fileName = "LootBank", menuName = "RPGPrototype/LootBank", order = 0)]
   public class LootBank : ScriptableObject
   {
+    [SerializeField] int size;
     [SerializeField] List<Item> savedItems = new List<Item>();
 
     public bool Empty => savedItems.Count == 0;
     public void AddLoot(List<Item> newItems)
     {
+      if (savedItems.Count + newItems.Count > size)
+      {
+        int capacityOverflow = savedItems.Count + newItems.Count - size;
+        savedItems.RemoveRange(0, capacityOverflow);
+      }
       savedItems.AddRange(newItems);
+      FindObjectOfType<SavingSystem>().Save("PlayerData", SaveType.PlayerSpecific);
     }
+
+    public void AddLoot(Item newItem)
+    {
+      savedItems.Add(newItem);
+    }
+
+    public void ClearLoot()
+    {
+      savedItems = new List<Item>();
+    }
+
     public List<Item> GetLoot()
     {
       List<Item> tmpList = savedItems;
-      //savedItems = null;
+      savedItems = new List<Item>();
       return tmpList;
+    }
+
+    public List<Item> ShowLoot()
+    {
+      return savedItems;
+    }
+
+    public float GetCapacityLevel()
+    {
+      return Mathf.InverseLerp(0, size, savedItems.Count);
     }
   }
 }
