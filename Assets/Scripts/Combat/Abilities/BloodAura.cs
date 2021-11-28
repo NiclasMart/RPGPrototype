86a969b6c4492b0a3d47c0344c57cbc0197a9346
Niclas Mart <salcintram07@web.de>
2021-11-28 10:33:50 +0100

    added and changed some ability effects

--- Assets/Scripts/Combat/Abilities/BloodAura.cs
+++ Assets/Scripts/Combat/Abilities/BloodAura.cs
@@ -8,10 +8,14 @@ namespace RPG.Combat
   public class BloodAura : Ability
   {
     [SerializeField] float duration;
+    [SerializeField] GameObject effect;
     Health health;
+    GameObject effectInstance;
     public override void CastAction()
     {
       if (health == null) health = data.source.GetComponent<Health>();
+      if (effectInstance == null) effectInstance = Instantiate(effect, transform);
+      
       float maxLife = health.GetMaxValue();
       float healPerSec = maxLife * (baseEffectValue / 100f);
 
@@ -21,12 +25,15 @@ namespace RPG.Combat
 
     IEnumerator ActiveHeal(float healPerSec)
     {
+      effectInstance.SetActive(true);
+
       float startTime = Time.time;
       while (Time.time < startTime + duration)
       {
         health.HealAbsolut((int)healPerSec / 2);
         yield return new WaitForSeconds(0.5f);
       }
+      effectInstance.SetActive(false);
     }
   }
 }
